c06f9de2e2c84a262661b405f2ad67385765e174
Lance Haynie <lhaynie@santaclarautah.gov>
2025-02-19 07:56:58 -0700

    jenkins

--- jenkins/03_prepare.groovy
+++ jenkins/03_prepare.groovy
@@ -1,8 +1,8 @@
 sh '''
 cp config/settings.yaml.example config/settings.yaml
 
-sed -i 's/^host=.*/host=localhost/' config/settings.yaml
-sed -i 's/^schema=.*/schema=spillmanapi/' config/settings.yaml
-sed -i 's/^user=.*/user=root/' config/settings.yaml
-sed -i 's/^password=.*/password=/' config/settings.yaml
+sed -i 's/^\(\s*host:\).*/\1 localhost/' config/settings.yaml
+sed -i 's/^\(\s*schema:\).*/\1 spillmanapi/' config/settings.yaml
+sed -i 's/^\(\s*user:\).*/\1 root/' config/settings.yaml
+sed -i 's/^\(\s*password:\).*/\1 ""/' config/settings.yaml
 '''
