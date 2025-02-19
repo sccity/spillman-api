a50efa13693177175d0b267085c8aff83d69d1a0
Lance Haynie <lhaynie@santaclarautah.gov>
2025-02-19 07:51:38 -0700

    jenkins

--- jenkins/01_initialize.groovy
+++ jenkins/01_initialize.groovy
@@ -3,7 +3,7 @@ echo "development" | su -c "/etc/init.d/mariadb start" root
 until mysqladmin ping --silent; do sleep 3; done
 echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '';" > setup.sql
 echo "FLUSH PRIVILEGES;" >> setup.sql
-echo "CREATE SCHEMA spillman-api;" >> setup.sql
+echo "CREATE SCHEMA spillmanapi;" >> setup.sql
 echo "development" | su -c "mysql -u root < setup.sql" root
 python3.10 -m venv venv
 . venv/bin/activate
--- jenkins/03_prepare.groovy
+++ jenkins/03_prepare.groovy
@@ -2,7 +2,7 @@ sh '''
 cp config/settings.yaml.example config/settings.yaml
 
 sed -i 's/^host=.*/host=localhost/' config/settings.yaml
-sed -i 's/^schema=.*/schema=influence360/' config/settings.yaml
+sed -i 's/^schema=.*/schema=spillmanapi/' config/settings.yaml
 sed -i 's/^user=.*/user=root/' config/settings.yaml
 sed -i 's/^password=.*/password=/' config/settings.yaml
 '''
