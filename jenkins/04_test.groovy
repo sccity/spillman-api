5cf534f2a72df5ff034415e687b248fd8e825757
Lance Haynie <lhaynie@santaclarautah.gov>
2025-02-19 12:19:45 -0700

    jenkins

--- jenkins/04_test.groovy
+++ jenkins/04_test.groovy
@@ -1,3 +1,4 @@
 sh '''
 python3.10 app.py --check-config
+yq -iY '.version = "$commit_hash"' spillman/version.yaml
 '''
--- jenkins/image.groovy
+++ jenkins/image.groovy
@@ -10,8 +10,6 @@ withCredentials([usernamePassword(credentialsId: 'docker-hub', usernameVariable:
         exit 1
     fi
 
-    yq -iY '.version = "$commit_hash"' spillman/version.yaml
-
     echo "Using Commit Hash: $commit_hash for Docker build"
     echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
