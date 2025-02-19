966a074959cc514c8e8d94686cdd69e04977f69b
Lance Haynie <lhaynie@santaclarautah.gov>
2025-02-19 10:37:43 -0700

    jenkins

--- jenkins/deploy.groovy
+++ jenkins/deploy.groovy
@@ -15,14 +15,19 @@ withCredentials([file(credentialsId: 'kubeconfig', variable: 'KUBECONFIG')]) {
     curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
     chmod +x kubectl
 
-    ./kubectl --kubeconfig $KUBECONFIG set image deployment/$DEPLOYMENT spillman-api=sccity/spillman-api:$commit_hash -n spillman
+    ./kubectl --kubeconfig $KUBECONFIG \
+        set image deployment/$DEPLOYMENT \
+        spillman-api=sccity/spillman-api:$commit_hash \
+        -n spillman
 
     if [ $? -ne 0 ]; then
         echo "Error: Kubernetes update failed!"
         exit 1
     fi
 
-    ./kubectl --kubeconfig $KUBECONFIG rollout status deployment/$DEPLOYMENT -n spillman
+    ./kubectl --kubeconfig $KUBECONFIG \
+        rollout status deployment/$DEPLOYMENT \
+        -n spillman
 
     if [ $? -ne 0 ]; then
         echo "Error: Kubernetes rollout failed!"
