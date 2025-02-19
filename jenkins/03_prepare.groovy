7fea2bc1334435aaf8264e71cd92d9d3c4e42ddf
Lance Haynie <lhaynie@santaclarautah.gov>
2025-02-19 08:21:45 -0700

    jenkins

--- jenkins/03_prepare.groovy
+++ jenkins/03_prepare.groovy
@@ -1,8 +1,8 @@
 sh '''
 cp config/settings.yaml.example config/settings.yaml
 
-yq eval '.database.host = "localhost"' -i config/settings.yaml
-yq eval '.database.schema = "spillmanapi"' -i config/settings.yaml
-yq eval '.database.user = "root"' -i config/settings.yaml
-yq eval '.database.password = ""' -i config/settings.yaml
+yq -iY '.database.host = "localhost"' config/settings.yaml
+yq -iY '.database.schema = "spillmanapi"' config/settings.yaml
+yq -iY '.database.user = "root"' config/settings.yaml
+yq -iY '.database.password = ""' config/settings.yaml
 '''
